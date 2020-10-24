5409f3d3b8a161cfc241013ef682f66fc2f42a18
Crowdbotics <team@crowdbotics.com>
2020-10-24 22:31:02 +0000

    Update models

--- backend/foodsapp_21926/wsgi.py
+++ backend/foodsapp_21926/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodsapp_21926.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodsapp_21926.settings")
 
 application = get_wsgi_application()
